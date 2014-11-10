2940b0c17469ff2dd622490afd4b35dd8b5160dd
OA Wu <comdan66@gmail.com>
2014-11-10 14:23:28 +0800

    Fix code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,56 @@
 class UsersController < ApplicationController
-  before_action :set_user, :only => [ :show, :edit, :update, :destroy]
+  before_action :set_user, only: [ :show, :edit, :update, :destroy]
 
   def index
     @users = User.all
-    @page_title = '使用者列表'
+    @page_title = "使用者列表"
   end
 
   def create
-    flash[:notice] = "user was successfully created"
-
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "user was successfully created"
       redirect_to :action => :index
     else
+      flash[:error] = "Create user error"
+      @error_messages = @user.errors.full_messages if @user.errors.any?
       render :action => :new
     end
   end
 
   def show
-    @page_title = @user.name + ' 的基本資料'
+    @page_title = "#{@user.name} 的基本資料"
   end
 
   def update
-    flash[:notice] = "user was successfully updated"
-
     if @user.update(user_params)
+      flash[:notice] = "user was successfully updated"
       redirect_to :action => :show, :id => @user
     else
-      render :action => :new
+      flash[:error] = "Update user error"
+      @error_messages = @user.errors.full_messages if @user.errors.any?
+      render :action => :edit
     end
   end
 
   def destroy
-    flash[:alert] = "user was successfully deleted"
-    
-    @user.destroy
+
+    if @user.destroy
+      flash[:notice] = "user was successfully deleted"
+    else
+      flash[:error] = "Delete user error"
+    end
+
     redirect_to users_path
   end
 
   def new
     @user = User.new
-    @page_title = '新增使用者'
+    @page_title = "新增使用者"
   end
 
   def edit
-    @page_title = '編輯' + @user.name + '的基本資料'
+    @page_title = "編輯 #{@user.name} 的基本資料"
   end
 
   private
